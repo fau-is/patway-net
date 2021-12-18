87b797a53d7560988881022447d86a72f7980db5
svwe <weinzierl.sven@web.de>
2021-12-18 11:17:37 +0100

    Removed code for bpi2011.

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='xprem',
+    name='patway-net',
     version='1.0.0',
     description='Explainable Predictions for Medical Pathways',
     long_description=readme,
-    author=['Mathias Kraus', 'Sven Weinzierl'],
-    author_email=['mathias.kraus@fau.de', 'sven.weinzierl@fau.de'],
-    url='https://github.com/fau-is/explainable-process-predictions-healthcare',
+    author=['Sandra Zilker'],
+    author_email=['sandra.zilker@fau.de'],
+    url='https://github.com/fau-is/xprem',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
