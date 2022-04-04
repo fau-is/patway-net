a865a60b88acc0ed4c5dca147a4360158dca5b9d
svwe <weinzierl.sven@web.de>
2022-04-04 17:31:00 +0200

    Added powertransformer for scaling label.

--- src/main_sim.py
+++ src/main_sim.py
@@ -7,7 +7,7 @@ import numpy as np
 import os
 from sklearn.preprocessing import PowerTransformer
 
-x_seqs, x_statics, y, _, seq_features, static_features = get_sim_data('Label', 'Simulation_data_5k.csv')
+x_seqs, x_statics, y, _, seq_features, static_features = get_sim_data('Label', 'Simulation_data_1k.csv')
 
 x_seq_final = np.zeros((len(x_seqs), 12, len(x_seqs[0][0])))
 x_stat_final = np.zeros((len(x_seqs), len(x_statics[0])))
@@ -19,8 +19,8 @@ y_final = np.array(y)  # .astype(np.int32)
 x_seq_final = torch.from_numpy(x_seq_final)
 x_stat_final = torch.from_numpy(x_stat_final)
 
-# pt = PowerTransformer()
-# y_final = pt.fit_transform(y_final.reshape(-1, 1))
+pt = PowerTransformer()
+y_final = pt.fit_transform(y_final.reshape(-1, 1))
 
 y_final = torch.from_numpy(y_final).reshape(-1)
 
