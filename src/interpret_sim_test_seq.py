a2de9ebb3263f5be67bb4bdcff9c8bb9c3c4b0c1
svwe <weinzierl.sven@web.de>
2022-04-11 17:30:04 +0200

    Added local and global plots for seq features.

--- src/interpret_sim_test_seq.py
+++ src/interpret_sim_test_seq.py
@@ -7,6 +7,8 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import PowerTransformer
+import seaborn as sns
+import pandas as pd
 
 model = torch.load(os.path.join("../model", f"model_sim"))
 
@@ -17,6 +19,7 @@ for i, x in enumerate(x_seqs):
     x_seq_final[i, :len(x), :] = np.array(x)
     x_stat_final[i, :] = np.array(x_statics[i])
 
+""""
 def slopee(x1,y1,x2,y2):
     eps = 0.01
     # x2 = 2; x1 = 1 --> +1 (fine)
@@ -25,7 +28,6 @@ def slopee(x1,y1,x2,y2):
     x = (y2 - y1) / ((x2 - x1) + 1.0 + eps)
     return x
 
-"""
 # seq_features = ["CRP"]
 # Print seq features (t x to t y)
 t_x = [1, 1, 5]
@@ -62,57 +64,90 @@ for t in range(0, len(t_x)):
             # fig1.savefig(f'../plots/{value}.png', dpi=100)
 
             # sns.kdeplot(data["x_x"], data["x_y"])
-"""
 
-
-# Print seq features (t x)
-
+# Print sequential features (local)
 effect_feature_values = []
-h_t_values = []
 
 for t in range(0, 12):
     for idx, value in enumerate(seq_features):
-        # x, out = model.plot_feat_seq_effect_custom(idx, -2, 2)
-        x, out, h_t, out_coef = model.plot_feat_seq_effect(idx, torch.from_numpy(x_seq_final[1, t, idx].reshape(1, 1, 1)).float())  # -1
+
+        x, out, h_t, out_coef = model.plot_feat_seq_effect(idx, torch.from_numpy(x_seq_final[0, t, idx].reshape(1, 1, 1)).float())
         x = x.detach().numpy().squeeze()
         out = out.detach().numpy()
-
         h_t_list = [torch.mean(h_t[i,:]).item() for i in range (0, h_t.shape[0])]
         h_t_final = sum(h_t_list) / len(h_t_list)
-
         out_coef_final = torch.mean(out_coef.reshape(-1)).item()
-
-
         print(f"Time step {t} --- Feature {value} --- Effect Value {torch.mean(torch.from_numpy(out.squeeze()))} --- Hidden state {h_t_final} --- Coef {out_coef_final}")
-        """
-        print("h_t")
-        print(h_t.reshape(1,-1))
-        print("out_coef")
-        print(out_coef.reshape(1,-1))
-
-        if value == "CRP" or value == "LacticAcid" or value == "Start":
-            plt.scatter(x, out)  # scatter plot
-        elif value == "IVA" or value == "IVL":
-            # todo: check bar plot
-            a, b = zip(set(x), set(np.squeeze(out)))
-            x = [list(a)[0], list(b)[0]]
-            out = [list(a)[1], list(b)[1]]
-            plt.bar(x, out)  # bar plot
-            plt.xticks(x, x)
-        else:
-            plt.plot(x, out)  # line plot
-    
-        plt.xlabel("Feature value")
-        plt.ylabel("Feature effect on model output")
-        plt.title(f"Sequential feature:{seq_features[idx]}")
-        fig1 = plt.gcf()
-        plt.show()
-        plt.draw()
-        fig1.savefig(f'../plots/{value}.png', dpi=100)
-        """
+"""
 
 """
-# Print stat features
+# 1) Print sequential features (local)
+effect_feature_values = []
+case = 2
+colors = ['blue', 'green', 'red', 'black', 'magenta']
+
+for idx, value in enumerate(seq_features):
+    effect_feature_values.append([])
+    for t in range(0, 12):
+        x, out, h_t, out_coef = model.plot_feat_seq_effect(idx, torch.from_numpy(
+            x_seq_final[case, t, idx].reshape(1, 1, 1)).float())
+        x = x.detach().numpy().squeeze()
+        out = out.detach().numpy()
+        effect_feature_values[-1].append(out[0][0])
+
+    plt.plot(list(range(1, 13)), effect_feature_values[idx], label=value, color=colors[idx])
+plt.xlabel("Time step")
+plt.ylabel("Feature effect on model output")
+plt.title(f"Change of static features for case: {case}")
+fig1 = plt.gcf()
+plt.legend()
+plt.xticks(np.arange(1, 13, 1))
+plt.show()
+plt.draw()
+fig1.savefig(f'../plots/seq_features_case_{case}.png', dpi=100)
+"""
+
+# 2) Print sequential features (local)
+
+cases = 100
+colors = ['blue', 'green', 'red', 'black', 'magenta']
+
+# for case in range(0, cases):
+effect_feature_values = []
+t_values = []
+feature_names = []
+for idx, value in enumerate(seq_features):
+
+    for t in range(0, 12):
+        x, out, h_t, out_coef = model.plot_feat_seq_effect(idx, torch.from_numpy(x_seq_final[0:cases, t, idx].reshape(-1, 1, 1)).float())
+        x = x.detach().numpy().squeeze()
+        out = out.detach().numpy()
+        effect_feature_values = effect_feature_values + list(out.squeeze())
+        t_values = t_values + [t+1] * cases
+        feature_names = feature_names + [value] * cases
+
+df = pd.DataFrame(list(zip(effect_feature_values, t_values, feature_names)), columns =['effect', 't', 'feature'])
+
+sns.stripplot(x="t", y="effect", hue="feature", data=df)
+plt.show()
+
+
+"""    
+plt.xlabel("Time step")
+plt.ylabel("Feature effect on model output")
+plt.title(f"Change of static features for case: {cases}")
+fig1 = plt.gcf()
+plt.legend()
+plt.xticks(np.arange(1, 13, 1))
+plt.show()
+plt.draw()
+fig1.savefig(f'../plots/seq_features_case_{cases}.png', dpi=100)
+"""
+
+
+
+"""
+# 3) Print static features (global)
 for idx, value in enumerate(static_features):
     # x, out = model.plot_feat_stat_effect_custom(idx, 0, 1)
     x, out = model.plot_feat_stat_effect(idx, torch.from_numpy(x_stat_final[:, idx].reshape(-1, 1)).float())
