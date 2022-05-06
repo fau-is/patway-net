f2ba6fe347d91150966e2cffa54310f95764ab34
svwe <weinzierl.sven@web.de>
2022-05-07 01:45:10 +0200

    Added seq plot for interpret sepsis.

--- src/interpret_sepsis.py
+++ src/interpret_sepsis.py
@@ -22,10 +22,10 @@ def delta(y2, y1):
 
 
 # Print seq features (t x to t y)
-t_x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-t_y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+t_x = list(range(0, 29))
+t_y = list(range(1, 30))
 
-for t in range(0, 11):  # num of transmissions
+for t in range(0, 29):  # num of transmissions
     for idx, feature in enumerate(seq_features):
 
         x_x, out_x, _, _ = model.plot_feat_seq_effect(idx, torch.from_numpy(
@@ -51,12 +51,6 @@ for t in range(0, 11):  # num of transmissions
             plt.clim(-0.03, 0.035)
         elif feature == 'LacticAcid':
             plt.clim(-0.03, 0.03)
-        elif feature == 'IVL':
-            plt.clim(-0.2, 0.2)
-        elif feature == 'IVA':
-            plt.clim(-0.2, 0.2)
-        elif feature == 'Start':
-            plt.clim(-0.03, 0.03)
         else:
             plt.clim(-0.5, 0.5)
         plt.xlim(-0.05, 1.05)
@@ -73,7 +67,8 @@ for t in range(0, 11):  # num of transmissions
         plt.close(fig1)
 """
 
-# Todo: update of scaling!
+# Todo: maybe correct intercept/ bias!
+"""
 # (2) Print static features (global)
 for idx, value in enumerate(static_features):
     # x, out = model.plot_feat_stat_effect_custom(idx, 0, 1)
@@ -98,3 +93,59 @@ for idx, value in enumerate(static_features):
     plt.draw()
     fig1.savefig(f'../plots/{value}.png', dpi=100)
     plt.close(fig1)
+"""
+
+
+#Todo: update of scaling!
+"""
+for t in range(0, 29):
+    for idx, value in enumerate(seq_features):
+        if value == "CRP" or value == "Leucocytes" or value == "LacticAcid":
+            # x, out = model.plot_feat_seq_effect_custom(idx, -2, 2)
+            x, out, h_t, out_coef = model.plot_feat_seq_effect(idx, torch.from_numpy(
+                x_seqs_final[:, t, idx].reshape(-1, 1, 1)).float())
+            x = x.detach().numpy().squeeze()
+            out = out.detach().numpy()
+            plt.scatter(x, out, color='steelblue')
+
+            # plt.xlim(-0.02, 1.02)
+            # plt.ylim(-0.05, 0.23)
+            plt.xlabel("Feature value")
+            plt.ylabel("Feature effect on model output")
+            plt.title("Sequential feature: %s ($t_{%s}$)" % (str(seq_features[idx]), str(t + 1)))
+            fig1 = plt.gcf()
+            plt.show()
+            plt.draw()
+            fig1.savefig(f'../plots/{value}_t{t + 1}.png', dpi=100)
+            plt.close(fig1)
+"""
+
+
+# (4) Print sequential features (local, no history)
+effect_feature_values = []
+case = 10
+colors = ['olivedrab', 'lightskyblue', 'steelblue', 'crimson', 'orange']
+plt.gca().set_prop_cycle(color=colors)
+
+for idx, value in enumerate(seq_features):
+    effect_feature_values.append([])
+    for t in range(0, 29):
+        x, out, h_t, out_coef = model.plot_feat_seq_effect(idx, torch.from_numpy(
+            x_seqs_final[case, t, idx].reshape(1, 1, 1)).float())
+        x = x.detach().numpy().squeeze()
+        out = out.detach().numpy()
+        effect_feature_values[-1].append(out[0][0])
+
+    # plt.ylim(-0.11, 0.21)
+    plt.plot(list(range(1, 30)), effect_feature_values[idx], label=value, linestyle='dashed', marker='o', markersize=4)
+plt.axhline(y=0, color='grey', linewidth=0.6)
+plt.xlabel("Time step")
+plt.ylabel("Feature effect on model output")
+plt.title(f"Feature effect over time of patient pathway {case}")
+fig1 = plt.gcf()
+plt.legend(loc='upper right', title='Sequential feature')  # adjust based on plot
+plt.xticks(np.arange(1, 30, 1))
+plt.show()
+plt.draw()
+fig1.savefig(f'../plots/seq_features_case_{case}.png', dpi=100)
+plt.close(fig1)
