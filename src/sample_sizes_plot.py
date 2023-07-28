7faedfdafd520cfd53215cbad48a5b8091b8f671
gy34bibo <sven.weinzierl@fau.de>
2023-07-28 14:40:57 +0200

    update sample sizes plot

--- src/sample_sizes_plot.py
+++ src/sample_sizes_plot.py
@@ -3,16 +3,20 @@ import matplotlib.pyplot as plt
 
 df = pd.DataFrame({
     "Ratio of training set" : [.0, .2, .4, .6, .8, 1],
-    "Avg" : [0.532047788, 0.612610353, 0.634283209, 0.663306835, 0.707324699, 0.734117295]
+    "Avg_pwn_one_inter" : [0.532047788, 0.612610353, 0.634283209, 0.663306835, 0.707324699, 0.734117295],
+    # "Avg_pwn_no_inter" : [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 })
 
-plt.plot(df["Ratio of training set"], df["Avg"], marker = "o", linestyle="--")
+plt.plot(df["Ratio of training set"], df["Avg_pwn_one_inter"], marker = "o", linestyle="--", label="PatWay-Net (with interaction)")
+# plt.plot(df["Ratio of training set"], df["Avg_pwn_no_inter"], color="grey", marker = "o", linestyle="--", label="PatWay-Net (no interaction)")
 
 x_labels = ["10%", "20%", "40%", "60%", "80%", "100%"]
 plt.xticks(df["Ratio of training set"], x_labels)
 
 y = [.5, .55, .6, .65, .7, .75]
 plt.yticks(y)
+
+# plt.legend(loc="lower right")
 
 plt.xlabel("Training sample size")
 plt.ylabel("Test $AUC_{ROC}$")
