66fd952b23a5068b4a544800565c81fe6f3a5796
gy34bibo <sven.weinzierl@fau.de>
2023-09-11 19:42:55 +0200

    revised sepsis interpretation plots

--- src/interpret_sepsis.py
+++ src/interpret_sepsis.py
@@ -15,6 +15,7 @@ number_interactions_seq = len(interactions_seq)
 
 x_seqs, x_statics, y, x_time_vals_final, seq_features, static_features = data.get_sepsis_data('Admission IC', 50, 3)
 x_seqs_final, x_statics_final, y_final = time_step_blow_up(x_seqs, x_statics, y, 50)
+case = -1
 
 """
 # (1) Print static features (global, history)
@@ -43,36 +44,41 @@ for idx, value in enumerate(static_features):
 
         if value in stat_numeric:
             # numerical
-            g = sns.lineplot(data=data, y="y", x="x", linewidth=2, color="steelblue", ax=axes[0])
+            g = sns.lineplot(data=data, y="y", x="x", linewidth=2, color="black", ax=axes[0])
             g.axhline(y=0, color="grey", linestyle="--")
 
         else:
             # categorical
-            g = sns.lineplot(data=data_agg, y="y_unique", x="x_unique", linewidth=2, drawstyle="steps-mid", color="steelblue", ax=axes[0])
+            g = sns.lineplot(data=data_agg, y="y_unique", x="x_unique", linewidth=2, drawstyle="steps-mid", color="black", ax=axes[0])
             g.axhline(y=0, color="grey", linestyle="--")
             axes[0].set_xticks([0, 1])
 
-        g1 = sns.barplot(data=data_agg, y="x_count", x="x_unique", color="steelblue", ax=axes[1])
+        g1 = sns.barplot(data=data_agg, y="x_count", x="x_unique", color="grey", ax=axes[1])
         g1.tick_params(axis="x", bottom=False, labelbottom=False)
 
-        axes[0].set_title(f"Static feature: {static_features[idx]}")
+        x_case = x_statics_final[case, idx]
+        y_case = torch.from_numpy(x_case.reshape(-1, 1)).float()
+        _, out_ = model.plot_feat_stat_effect(idx, y_case)
+        out_ = out_.detach().numpy()[0]
+
+        g.plot(x_case, out_ + out_delta, marker='x', ms=10, mew=2, ls='none', color='red')
+
+        axes[0].set_title(f"Static medical indicator: {static_features[idx]}")
         axes[0].set_xlabel(None)
         axes[0].grid(True)
-        axes[0].set_ylabel("Feature effect on model output")
+        axes[0].set_ylabel("Effect on prediction")
 
         axes[1].set_ylabel(None)
-        axes[1].set_xlabel("Feature value")
+        axes[1].set_xlabel(f"{static_features[idx]}")
 
         f.tight_layout(pad=1.0)
-        plt.savefig(f'../plots/sepsis/stat_feat_{value}.pdf', dpi=100, bbox_inches="tight")
+        plt.savefig(f'../plots/sepsis/stat_feat_{value}.png', dpi=100, bbox_inches="tight")
         plt.show()
         plt.close(f)
-"""
 
 
-"""
+
 # (2) Print sequential features (local, history, manipulated sequence)
-case = -1
 for t in range(1, 13):
     for idx, value in enumerate(seq_features):
         if value == "Leucocytes" or value == "LacticAcid" or value == "CRP":
@@ -114,7 +120,7 @@ for t in range(1, 13):
 
             data = pd.DataFrame({'x': x, 'y': out})
 
-            g = sns.lineplot(data=data, y="y", x="x", linewidth=2, color="steelblue")
+            g = sns.lineplot(data=data, y="y", x="x", linewidth=2, color="black")
             g.axhline(y=0, color="grey", linestyle="--")
 
             # add x value of step n
@@ -123,27 +129,26 @@ for t in range(1, 13):
             _, out_, _, _ = model.plot_feat_seq_effect(idx, y_step_n, history=True)
             out_ = out_.detach().numpy()[0]
 
-            plt.plot(x_step_n, out_+out_delta, marker='s', ms=4, mew=2, ls='none', color='black')
+            g.plot(x_step_n, out_ + out_delta, marker='x', ms=10, mew=2, ls='none', color='red')
 
             plt.grid(True)
 
-            plt.xlabel("Feature value")
-            plt.ylabel("Feature effect on model output")
-            plt.title("Sequential feature: %s" % (str(seq_features[idx])))
+            plt.xlabel(f"{str(seq_features[idx])} value")
+            plt.ylabel("Effect on prediction")
+            plt.title("Sequential medical indicator: %s" % (str(seq_features[idx])))
             f = plt.gcf()
             plt.show()
             plt.draw()
-            f.savefig(f'../plots/sepsis/seq_feat_{value}_{t}.pdf', dpi=100)
+            f.savefig(f'../plots/sepsis/seq_feat_{value}_{t}.png', dpi=100)
             plt.close(f)
 
 # prediction at step n
 # print(torch.sigmoid(model(torch.from_numpy(x_seqs_final[case, :, :].reshape(1, 50, 16)),
 #                          torch.from_numpy(x_statics_final[case, :].reshape(1, 23)))))
-"""
+
 
 
 # (3) Print sequential feature transition (local, history, manipulated sequence)
-case = -1
 for t in range(3, 13):
     for idx, value in enumerate(seq_features):
 
@@ -186,9 +191,10 @@ for t in range(3, 13):
                     output2 = out_n[j]
                     diffs[i, j] = float(output2) - float(output1)
 
-            plt.imshow(diffs, cmap='viridis', interpolation='nearest', origin='lower')
-            cbar = plt.colorbar(label='$\Delta$ Feature effect')
-
+            cmap = plt.cm.get_cmap('RdBu')
+            cmap = cmap.reversed()
+            plt.imshow(diffs, cmap=cmap, interpolation='nearest', origin='lower', vmin=-3.5, vmax=3.5)
+            cbar = plt.colorbar(label="Change of effect on prediction")
 
             ticks = np.linspace(0, 1, 5)
             tick_indices = np.linspace(0, len(x_n) - 1, len(ticks)).astype(int)
@@ -196,14 +202,20 @@ for t in range(3, 13):
             plt.xticks(ticks=tick_indices, labels=ticks)
             plt.yticks(ticks=tick_indices, labels=ticks)
 
-            plt.xlabel("Feature value $(t=n-1)$", )
-            plt.ylabel("Feature value $(t=n)$")
-            plt.title(f"Sequential feature: {seq_features[idx]}")
+            # add current patient
+            x_n = np.ravel(x_seqs_final[case, t, idx].reshape(1, 1, 1))[0]
+            x_n_min_1 = np.ravel(x_seqs_final[case, t - 1, idx].reshape(1, 1, 1))[0]
+            plt.plot(x_n_min_1*(200-1), x_n*(200-1), marker='x', ms=10, mew=2, ls='none', color='black')
+
+            plt.xlabel(f"{value} value (previous)", )
+            plt.ylabel(f"{value} value (current)")
+            plt.title(f"Sequential medical indicator: {seq_features[idx]}")
             f = plt.gcf()
             plt.show()
             plt.draw()
-            f.savefig(f'../plots/sepsis/seq_feat_diffs_{value}_{t}.pdf', dpi=100, bbox_inches="tight")
+            f.savefig(f'../plots/sepsis/seq_feat_diffs_{value}_{t}.png', dpi=100, bbox_inches="tight")
             plt.close(f)
+"""
 
 """
 # (4) Print sequential feature interactions (global, no history)
@@ -244,15 +256,15 @@ if number_interactions_seq > 0:
         fig1 = plt.gcf()
         plt.show()
         plt.draw()
-        fig1.savefig(f'../plots/sepsis/seq_feat_inter_{interactions_seq[idx][0]}-{interactions_seq[idx][1]}.pdf',
+        fig1.savefig(f'../plots/sepsis/seq_feat_inter_{interactions_seq[idx][0]}-{interactions_seq[idx][1]}.png',
                      dpi=100, bbox_inches="tight")
         plt.close(fig1)
 """
 
-"""
+
 # (5) Print sequential feature (local, history)
-case = -1
 max_len = 12
+last_step = 4
 seq_features_rel = ['Leucocytes', 'CRP', 'LacticAcid']
 
 for idx, value in enumerate(seq_features_rel):
@@ -266,7 +278,6 @@ for idx, value in enumerate(seq_features_rel):
         plt.rc('axes', titlesize=16)
 
         for t in range(1, max_len+1):
-
 
             x, out, h_t, out_coef = model.plot_feat_seq_effect(
                 idx, torch.from_numpy(x_seqs_final[case, 0:t, idx].reshape(1, t, 1)).float(), history=True)
@@ -312,20 +323,29 @@ for idx, value in enumerate(seq_features_rel):
 
         data = pd.DataFrame({'x': list_time, 'y': list_value, 'z': list_effect})
 
-        g = sns.lineplot(data=data, y="y", x="x", linewidth=2, color="steelblue")
+        g = sns.lineplot(data=data[0:last_step], y="y", x="x", linewidth=2, color="black", zorder=1, linestyle="--")
         g.axhline(y=0, color="grey", linestyle="--")
 
-        sc = plt.scatter(list(range(1, max_len+1)), data_feature_values, c=effect_feature_values, cmap='viridis')
+        cmap = plt.cm.get_cmap('RdBu')
+        cmap = cmap.reversed()
 
-        plt.colorbar(sc, label='Feature effect on model output')
+        sc = plt.scatter(list(range(1, max_len + 1))[0:last_step], data_feature_values[0:last_step], c=effect_feature_values[0:4], cmap=cmap,
+                         zorder=2, vmin=-2.5, vmax=2.5, edgecolors='black', s=100)
+        plt.colorbar(sc, label='Effect on prediction')
+
         plt.grid(True)
-        plt.ylabel("Feature value")
-        plt.xlabel("Time step")
-        plt.title("Sequential feature effect over time: %s" % (str(seq_features[idx])))
+        plt.ylabel(f"{str(seq_features[idx])} value")
+        plt.xlabel("Measurement date")
+
+        plt.xticks(np.arange(0, 12, step=1), ["2014-09-18 13:46", "2014-09-18 13:55", "2014-09-18 13:56", "2014-09-18 14:11",
+                                              "2014-09-18 14:12", "2014-09-18 14:14", "2014-09-18 15:44", "2014-09-18 17:57",
+                                              "2014-09-18 17:59", "2014-09-18 18:15", "2014-09-18 18:17",
+                                              "2014-09-18 18:18"][0:last_step]+["..."]*(max_len-last_step), rotation=20)
+        plt.title(f"Recent development of {str(seq_features[idx])} values")
 
         f = plt.gcf()
+        f.tight_layout()
         plt.show()
-        f.savefig(f'../plots/sepsis/seq_feat_case_{case}_{value}_time.pdf', dpi=100, bbox_inches="tight")
-        plt.close(f)
+        f.savefig(f'../plots/sepsis/seq_feat_case_{case}_{value}_time.png', dpi=100, bbox_inches="tight")
 
-"""
+        plt.close(f)
