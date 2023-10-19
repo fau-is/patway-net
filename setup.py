1200b2c368b6d0a7b6df17bbaff0b84870ea8ab6
svwe <weinzierl.sven@web.de>
2023-10-19 14:04:29 +0200

    final update repo after first hcms revivsion

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ with open('LICENSE') as f:
 setup(
     name='patway-net',
     version='1.0.0',
-    description='Explainable Predictions for Medical Pathways',
+    description='Interpretable Predictions for Patient Pathways',
     long_description=readme,
-    author=['Sandra Zilker'],
-    author_email=['sandra.zilker@fau.de'],
+    author="Sandra Zilker, Sven Weinzierl, Mathias Kraus, Patrick Zschech, Martin Matzner",
+    author_email="sandra.zilker@fau.de, sven.weinzierl@fau.de, mathias.kraus@fau.de, patrick.zschech@fau.de, martin.matzner@fau.de",
     url='https://github.com/fau-is/patway-net',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
